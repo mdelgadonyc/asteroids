fd7d65500667218fff37f70219ee2483ec076023
mdelgadonyc <moises.f.delgado@gmail.com>
2024-10-07 21:54:44 -0400

    [feat] Add left and right player rotation functionality

--- player.py
+++ player.py
@@ -1,6 +1,6 @@
 from circleshape import CircleShape
 import pygame
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_TURN_SPEED
 
 class Player(CircleShape):
     rotation = 0
@@ -16,10 +16,20 @@ class Player(CircleShape):
         b = self.position - forward * self.radius - right
         c = self.position - forward * self.radius + right
         return [a, b, c]
+
+    def rotate(self, dt):
+        self.rotation = self.rotation + PLAYER_TURN_SPEED * dt
+
         
     def draw(self, screen):
         points = self.triangle()
         pygame.draw.polygon(screen, pygame.Color("white"), self.triangle(), 2)
 
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
 
-        
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+
+        if keys[pygame.K_d]:
+            self.rotate(dt)
